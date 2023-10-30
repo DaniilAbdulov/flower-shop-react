ae3505c991e0bd0c1d430154e0fd908ea1c28b92
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-10-30 19:14:54 +0500

    authModule

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,21 @@
+require("dotenv").config();
 const express = require("express");
-const { join } = require("path");
 const cors = require("cors");
+const router = require("./routes/index");
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use("/api", router);
 
-// app.use("/api/auth", require("./routes/auth"));
-app.use("/api/products", require("./routes/products"));
-app.use("/api/product", require("./routes/productPage"));
+const start = async () => {
+    try {
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+};
 
-// app.get("/", (req, res) => {
-
-//     res.sendFile(join(__dirname, "..", "frontend", "public", "index.html"));
-// });
-
-const port = 4000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+start();
