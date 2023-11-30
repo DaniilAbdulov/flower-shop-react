e3793e68dbb30cd8b73c8f427b8549484275f810
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-11-30 11:18:01 +0500

    Update useImageLoader.js

--- frontend/src/hooks/useImageLoader.js
+++ frontend/src/hooks/useImageLoader.js
@@ -1,1 +1,20 @@
+import {useState,useEffect} from 'react';
+const useImageLoader = (imgUrl) =>{
+  const [imgIsLoading,setImgIsLoading] = useState(false);
+  const [hasError,setHasError] = useState(false);
 
+  useEffect(()=>{
+    const image = new Image();
+    image.onload = () => {setImgIsLoading(true)};
+    image.onerror = () => {setHasError(true)};
+    image.src = imgUrl;
+    return () => {
+      image.onload = null;
+      image.onerror = null;
+    };
+  },[imgUrl]);
+  return {imgIsLoading, hasError};
+}
+export default useImageLoader;
+
+//const {imgIsLoading, hasError} = useImageLoader(props.data.img);
