3e58ee17a61d472cfcaf688b8cfdd209a79c13c1
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-12-04 17:32:29 +0500

    delete spaces

--- backend/controllers/toggleFavoriteController.js
+++ backend/controllers/toggleFavoriteController.js
@@ -5,25 +5,20 @@ class toggleFavoriteController {
         try {
             const productId = parseInt(req.body.params.productId);
             const userId = req.user.id;
-
             if (!productId || !userId) {
                 throw new Error("Некорректный id продукта или пользователя");
             }
-
             const thisProductIsLikedByThisUser = await pool.query(
                 "SELECT users_id, product_id FROM favorites WHERE users_id = $1 AND product_id = $2;",
                 [userId, productId]
             );
-
             if (thisProductIsLikedByThisUser.rowCount > 0) {
                 return res.status(500).json({ message: "Уже лайкнут" });
             }
-
             await pool.query(
                 "INSERT INTO favorites (users_id, product_id) VALUES ($1, $2);",
                 [userId, productId]
             );
-
             return res
                 .status(200)
                 .json({ message: "Товар добавлен в избранное" });
@@ -33,27 +28,22 @@ class toggleFavoriteController {
                 .json({ message: "Ошибка добавления товара в избранное" });
         }
     }
-
     async deleteFromFavorites(req, res) {
         try {
             const productId = parseInt(req.query.productId);
             const userId = req.user.id;
-
             if (!productId || !userId) {
                 throw new Error("Некорректный id продукта или пользователя");
             }
-
             const thisProductIsLikedByThisUser = await pool.query(
                 "SELECT users_id, product_id FROM favorites WHERE users_id = $1 AND product_id = $2;",
                 [userId, productId]
             );
-
             if (thisProductIsLikedByThisUser.rowCount > 0) {
                 const tryToDelete = await pool.query(
                     "DELETE FROM favorites WHERE users_id = $1 AND product_id = $2",
                     [userId, productId]
                 );
-
                 if (tryToDelete.rowCount) {
                     return res
                         .status(200)
@@ -62,7 +52,6 @@ class toggleFavoriteController {
                     throw new Error("Ошибка удаления из избранного");
                 }
             }
-
             return res
                 .status(200)
                 .json({ message: "deleteFromFavorites path is work" });
