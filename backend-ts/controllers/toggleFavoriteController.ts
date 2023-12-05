3a5071861e6c9bc47298187c0bf239cf50b2aa14
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-12-05 13:56:27 +0500

    toggleFavorite

--- backend-ts/controllers/toggleFavoriteController.ts
+++ backend-ts/controllers/toggleFavoriteController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { pool } from "../db";
 
-class toggleFavoriteController {
+class ToggleFavoriteController {
     async addToFavorites(req: Request, res: Response) {
         try {
             const productId: number = parseInt(req.body.params.productId);
@@ -9,26 +9,34 @@ class toggleFavoriteController {
             if (!productId || !userId) {
                 throw new Error("Некорректный id продукта или пользователя");
             }
-            const thisProductIsLikedByThisUser = await pool.query(
-                "SELECT users_id, product_id FROM favorites WHERE users_id = $1 AND product_id = $2;",
-                [userId, productId]
-            );
-            if (thisProductIsLikedByThisUser.rowCount > 0) {
+
+            const query = {
+                text: "SELECT users_id, product_id FROM favorites WHERE users_id = $1 AND product_id = $2",
+                values: [userId, productId],
+            };
+            const { rowCount: productLikedCount } = await pool.query(query);
+
+            if (productLikedCount > 0) {
                 return res.status(500).json({ message: "Уже лайкнут" });
             }
-            await pool.query(
-                "INSERT INTO favorites (users_id, product_id) VALUES ($1, $2);",
-                [userId, productId]
-            );
+
+            const insertQuery = {
+                text: "INSERT INTO favorites (users_id, product_id) VALUES ($1, $2)",
+                values: [userId, productId],
+            };
+            await pool.query(insertQuery);
+
             return res
                 .status(200)
                 .json({ message: "Товар добавлен в избранное" });
         } catch (error) {
-            return res
-                .status(500)
-                .json({ message: "Ошибка добавления товара в избранное" });
+            return res.status(500).json({
+                message: "Ошибка добавления товара в избранное",
+                error: error.message,
+            });
         }
     }
+
     async deleteFromFavorites(req: Request, res: Response) {
         try {
             const productId: number = Number(req.query.productId);
@@ -36,16 +44,23 @@ class toggleFavoriteController {
             if (!productId || !userId) {
                 throw new Error("Некорректный id продукта или пользователя");
             }
-            const thisProductIsLikedByThisUser = await pool.query(
-                "SELECT users_id, product_id FROM favorites WHERE users_id = $1 AND product_id = $2;",
-                [userId, productId]
-            );
-            if (thisProductIsLikedByThisUser.rowCount > 0) {
-                const tryToDelete = await pool.query(
-                    "DELETE FROM favorites WHERE users_id = $1 AND product_id = $2",
-                    [userId, productId]
+
+            const query = {
+                text: "SELECT users_id, product_id FROM favorites WHERE users_id = $1 AND product_id = $2",
+                values: [userId, productId],
+            };
+            const { rowCount: productLikedCount } = await pool.query(query);
+
+            if (productLikedCount > 0) {
+                const deleteQuery = {
+                    text: "DELETE FROM favorites WHERE users_id = $1 AND product_id = $2",
+                    values: [userId, productId],
+                };
+                const { rowCount: deletedCount } = await pool.query(
+                    deleteQuery
                 );
-                if (tryToDelete.rowCount) {
+
+                if (deletedCount) {
                     return res
                         .status(200)
                         .json({ message: "Удалено из избранного" });
@@ -53,15 +68,17 @@ class toggleFavoriteController {
                     throw new Error("Ошибка удаления из избранного");
                 }
             }
+
             return res
                 .status(200)
                 .json({ message: "Товар удален из избранного" });
         } catch (error) {
-            return res
-                .status(500)
-                .json({ message: "Ошибка удаления товара из избранного" });
+            return res.status(500).json({
+                message: "Ошибка удаления товара из избранного",
+                error: error.message,
+            });
         }
     }
 }
 
-export default new toggleFavoriteController();
+export default new ToggleFavoriteController();
