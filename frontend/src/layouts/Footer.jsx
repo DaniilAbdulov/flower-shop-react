1482a4616a4ecdb4c66e17bc42ac7593d4b1cdc5
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-12-05 18:02:00 +0500

    mergebranchs

--- frontend/src/layouts/Footer.jsx
+++ frontend/src/layouts/Footer.jsx
@@ -5,11 +5,10 @@ function Footer() {
             <footer className="footer">
                 <div className="footer__row">
                     <ul className="footer__links">
-                        <li className="footer__link">Github</li>
-                        <li className="footer__link">Github</li>
-                        <li className="footer__link">Github</li>
-                        <li className="footer__link">Github</li>
-                        <li className="footer__link">Github</li>
+                        <li className="footer__link">Link One</li>
+                        <li className="footer__link">Link Two</li>
+                        <li className="footer__link">Link Three</li>
+                        <li className="footer__link">Link Four</li>
                     </ul>
                 </div>
             </footer>
